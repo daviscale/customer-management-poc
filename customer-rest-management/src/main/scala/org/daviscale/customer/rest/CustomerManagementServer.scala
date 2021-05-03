56840781fe697c146b00ac2366c978e6122bbcf1
Cale Davis <daviscale@gmail.com>
2021-05-03 08:50:14 -0400

    stubbed out CustomerManagementServer with a 'hello world' endpoint

---
+++ customer-rest-management/src/main/scala/org/daviscale/customer/rest/CustomerManagementServer.scala
@@ -0,0 +1,34 @@
+package org.daviscale.customer
+package rest
+
+import akka.actor._
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
+import scala.io.StdIn
+
+object CustomerManagementServer {
+
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("customer-management-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
